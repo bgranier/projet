d1d131938a24605674097435899b088911aea4ef
bgranier <bernard.granier@sfr.fr>
2016-09-20 23:47:32 +0200

    ajout d'un main

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: bgranier
+ *
+ * Created on 20 septembre 2016, 23:46
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout<<"hello";
+
+    return 0;
+}
+
